d56cf45a6cfd5aaee26affd76c66a046fb8546d6
bytheroom <81925748+bytheroom@users.noreply.github.com>
2021-09-20 14:42:35 +0300

    Added JSDoc to functions

--- functions.js
+++ functions.js
@@ -19,18 +19,57 @@ function switchPreview(presentation) {}
 
 function undo(history, presentation) {}
 
+/**
+ * Отменить последнее действие
+ * @param {history} history - История действий
+ * @return {presentation} Презентация
+ */
 function redo(history, presentation) {}
 
+/**
+ * Добавить новый слайд в презентацию
+ * @param {slides} slides - Слайды
+ * @return {presentation} your project
+ */
+function addSlide(slides) {}
 
-function addSlide(presentation) {}
+/**
+ * Удалить все выделенные слайды
+ * @param {slides}
+ * @param {selectedSlideId}
+ * @return {presentation} presentation - your project
+ */
+function removeSlide(slides, selectedSlideId) {}
 
-function removeSlide(presentation, slideId) {}
-
+/**
+ * Перейти на нажатый слайд
+ * @param {selectedSlideId}
+ * @param {slideId}
+ * @return {presentation} presentation - your project
+ */
 function switchSlide(selectedSlideId, slideId) {}
 
-
+/**
+ * Установить фоновое изображение
+ * @param {slideId}
+ * @param {background}
+ * @return {presentation} presentation - your project
+ */
 function setBackground(slideId, background) {} //?
 
+/**
+ * Добавить объект на слайд
+ * @param {objectType}
+ * @param {elements}
+ * @return {presentation} presentation - your project
+ */
 function addObject(objectType, elements) {}
 
+/**
+ * Изменить свойства объекта
+ * @param {object}
+ * @param {propertyType}
+ * @param {value}
+ * @return {presentation} presentation - your project
+ */
 function changeObject(object, propertyType, value) {}
