07167005c443b233742ef41cbbf4db321b820cbc
Safarov_Tahir <79829434+SafarovT@users.noreply.github.com>
2021-09-20 14:47:37 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,75 +1,111 @@
 /**
- * @param{Presentation} presentation
- * @return{Presentation}
- * @param{History} history
- * @return{Histoty}
- */
+ * Изменяет название презентации
+ * @param {Presentation} presentation
+ * @param {string} title
+ * @return {Presentation}
+*/
 
 
 function changeTitle(presentation, title) {}
 
+/**
+ * Сохраняет презенацию в виде json файла
+ * @param {Presentation} presentation
+ * @return {File}
+*/
+
 function saveDoc(presentation) {}
+
+/**
+ * Загружает презентацию в приложение
+ * @param {Presentation} presentation
+ * @param {File} presentationFile
+ * @return {Presentation}
+*/
 
 function uploadDoc(presentation, presentationFile) {}
 
+/**
+ * Экспортирует презентацию в виде pdf
+ * @param {Presentation} presentation
+ * @return {File}
+*/
+
 function exportDoc(presentation) {}
+
+/**
+ * Изменяет режим с редактирование презентации на просмотр, или наоборот
+ * @param {Presentation} presentation
+ * @param {Boolean} previewOn
+ * @return {Presentation}
+*/
 
 function switchPreview(presentation) {}
 
+/**
+ * Отменяет предыдущее действие
+ * @param {History} history
+ * @param {Presentation} Presentation
+ * @return {Presentation}
+*/
 
 function undo(history, presentation) {}
 
 /**
  * Отменить последнее действие
  * @param {history} history - История действий
- * @return {presentation} Презентация
+ * @return {presentation}
  */
+
 function redo(history, presentation) {}
 
 /**
  * Добавить новый слайд в презентацию
  * @param {slides} slides - Слайды
- * @return {presentation} your project
+ * @return {slides}
  */
+
 function addSlide(slides) {}
 
 /**
  * Удалить все выделенные слайды
- * @param {slides}
- * @param {selectedSlideId}
- * @return {presentation} presentation - your project
+ * @param {slides} slides
+ * @param {selectedSlideId} selectedSlideId
+ * @return {slides}
  */
+
 function removeSlide(slides, selectedSlideId) {}
 
 /**
  * Перейти на нажатый слайд
- * @param {selectedSlideId}
- * @param {slideId}
- * @return {presentation} presentation - your project
+ * @param {selectedSlideId} selectedSlideId
+ * @param {slideId} slideId
+ * @return {slides} presentation - your project
  */
+
 function switchSlide(selectedSlideId, slideId) {}
 
 /**
  * Установить фоновое изображение
- * @param {slideId}
- * @param {background}
- * @return {presentation} presentation - your project
+ * @param {slideId} slideId
+ * @param {string} background
+ * @return {slide}
  */
+
 function setBackground(slideId, background) {} //?
 
 /**
  * Добавить объект на слайд
- * @param {objectType}
- * @param {elements}
- * @return {presentation} presentation - your project
+ * @param {element}
+ * @return {elemenents}
  */
-function addObject(objectType, elements) {}
+function addObject(element, elements) {}
 
 /**
  * Изменить свойства объекта
- * @param {object}
- * @param {propertyType}
- * @param {value}
- * @return {presentation} presentation - your project
+ * @param {element} element
+ * @param {property} propertyType
+ * @param {string} value 
+ * @return {element}
  */
 function changeObject(object, propertyType, value) {}
