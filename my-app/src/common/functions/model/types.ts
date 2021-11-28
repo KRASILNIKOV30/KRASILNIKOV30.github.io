9b41c333d988bcb3f66073684838711bd01f9545
Safarov_Tahir <salarovsmv@gmail.com>
2021-11-28 16:23:05 +0300

    Сделал заготовку для SlideEditor

--- my-app/src/common/functions/model/types.ts
+++ my-app/src/common/functions/model/types.ts
@@ -1,9 +1,11 @@
+import { url } from "inspector"
+
 type Editor = {
     presentation: Presentation;
     history: History;
     statePreview: boolean;
     currentSlideIds: Array<string>
-}
+} 
 
 type History = {
     undoStack: Array<Presentation>;
@@ -29,7 +31,7 @@ type SlideElement = {
     position: Position; 
     size: Size; 
     image?: ImageType;
-    textProps: TextType;
+    textProps?: TextType;
     figure?: FigureType
 }
 
@@ -45,7 +47,7 @@ type Size = {
 
 type ImageType = {
     urlImage: string;
-    ImageType: "url" | "Base64";
+    imageType: "url" | "Base64";
     ext: string
 }
 
@@ -63,4 +65,104 @@ type FigureType = {
     fillColor: string
 }
 
-export type { TextType, SlideElement, Slide, Presentation, History, Editor };
+const editor: Editor = {
+    presentation: {
+        title: "Braviypresentation",
+        slides: [
+            {
+                slideId: "0",
+                elements: [
+                    {
+                        elementId: "0",
+                        elementType: "text",
+                        position: {
+                            x: 234,
+                            y: 111
+                        },
+                        size: {
+                            width: 50,
+                            height: 50
+                        },
+                        textProps: {
+                            textColor: "#000000",
+                            bgColor: "#ff0000",
+                            textValue: "Hello Kerim!",
+                            fontSize: "16px",
+                            fontWeight: "regular"
+                        }
+                    },
+                    {
+                        elementId: "1",
+                        elementType: "figure",
+                        position: {
+                            x: 32,
+                            y: 64
+                        },
+                        size: {
+                            width: 356,
+                            height: 93
+                        },
+                        figure: {
+                            form: "rectangle",
+                            strokeColor: "#d0ff00",
+                            fillColor: "#00ff2a"
+                        }
+                    },
+                    {
+                        elementId: "2",
+                        elementType: "figure",
+                        position: {
+                            x: 564,
+                            y: 455
+                        },
+                        size: {
+                            width: 50,
+                            height: 50
+                        },
+                        figure: {
+                            form: "triangle",
+                            strokeColor: "#00ff2a",
+                            fillColor: "#d0ff00"
+                        }
+                    }
+                ],
+                background: "#FFFFFF",
+                backgroundType: 'Base64',
+                selectedElementsIds: []
+            }, 
+            {
+                slideId: "1",
+                elements: [
+                    {
+                        elementId: "0",
+                        elementType: "image",
+                        position: {
+                            x: 54,
+                            y: 405
+                        },
+                        size: {
+                            width: 450,
+                            height: 59
+                        },
+                        image: {
+                            urlImage: "./assets/image",
+                            imageType: "url",
+                            ext: 'svg'
+                        }
+                    }
+                ],
+                background: "#000FFF",
+                backgroundType: 'Base64',
+                selectedElementsIds: ['0']
+            }
+        ]
+    },
+    history: {
+        undoStack: [],
+        redoStack: []
+    },
+    statePreview: false,
+    currentSlideIds: ['0']
+};
+
+export type { TextType, SlideElement, Slide, Presentation, History, Editor, editor };
--- my-app/src/slideEditor/slideEditor.tsx
+++ my-app/src/slideEditor/slideEditor.tsx
@@ -1,4 +1,14 @@
-const SlideEditor = () => {
-    return null
-}    
-export {SlideEditor}
+import React from 'react';
+
+function SlideEditor() {
+
+    return (
+        <div className="slide-container">
+            <div className="slide">
+                
+            </div>
+        </div>
+    )
+}
+
+export { SlideEditor }
