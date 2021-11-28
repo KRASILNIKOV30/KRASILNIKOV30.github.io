5badf29d1386dc30482583d3002441e806e684c8
Safarov_Tahir <salarovsmv@gmail.com>
2021-11-28 16:12:13 +0300

    Сделал заготовку для SlideEditor

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,32 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css'
-import { Palette } from './common/Palette/Palette';
-import { Button } from "./common/Button/Button";
-import { DropDown } from "./common/DropDown/DropDown";
-import { Input } from "./common/Input/input";
-import { ToolBar } from './toolBar/ToolBar';
-import { SlideEditor } from './slideEditor/slideEditor';
-import { SideBar } from './sideBar/sideBar';
-
-ReactDOM.render(
-    <div className = 'main-container'>
-        <div className = 'tool-bar-container'>
-            <ToolBar/>
-        </div>
-        <div className = 'pres-viewer'>
-            <div className = 'side-bar-container'>
-                <SideBar/>
-            </div>
-            <div className = 'slide-editor-container'>
-                <SlideEditor/>
-            </div>
-        </div>
-    </div>,
-    document.getElementById('root')
-)
-=======
 
 import './index.css';
 import { getEditor, addEditorChangeHandler } from './common/functions/model/editor';
@@ -43,4 +16,3 @@ function render() {
 
 addEditorChangeHandler(render)
 render()
->>>>>>> React-start
