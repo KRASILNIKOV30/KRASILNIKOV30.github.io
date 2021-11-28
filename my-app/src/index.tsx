6c9a7e633132d49a29092d04565ea2c0b245c0b4
KRASILNIKOV30 <bkrasilnikov3006@gmail.com>
2021-11-28 20:53:51 +0300

    Исправил ошибки

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import { getEditor, addEditorChangeHandler } from './common/functions/model/editor';
-import App from './app';
+import App from './App';
 
 function render() {
     ReactDOM.render(
